5c17857d74df6c1f669fd336fedb1fac2f4b6b62
Alvin <alvinlhang@gmail.com>
2024-10-01 16:58:06 -0700

    added theme

--- drocsid/app/(main)/(routes)/page.tsx
+++ drocsid/app/(main)/(routes)/page.tsx
@@ -1,20 +1,32 @@
-'use client'
+// 'use client'
 
-import { SignInButton, SignOutButton, useAuth } from '@clerk/nextjs'
+// import { SignInButton, SignOutButton, useAuth } from '@clerk/nextjs'
+
+// export default function Home() {
+//   const { sessionId } = useAuth()
+
+//   const buttonClass =
+//     'inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition text-center'
+
+//   return !sessionId ? (
+//     <div className={buttonClass}>
+//       <SignInButton />
+//     </div>
+//   ) : (
+//     <div className={buttonClass}>
+//       <SignOutButton signOutOptions={{ sessionId }} />
+//     </div>
+//   )
+// }
+
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  const { sessionId } = useAuth()
-
-  const buttonClass =
-    'inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition text-center'
-
-  return !sessionId ? (
-    <div className={buttonClass}>
-      <SignInButton />
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
-  ) : (
-    <div className={buttonClass}>
-      <SignOutButton signOutOptions={{ sessionId }} />
-    </div>
-  )
+  );
 }
