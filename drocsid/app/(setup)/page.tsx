95acd0f920c998d25ac76cb7bd9743ae571d78fd
Bryan Bergenholtz <116790009+YanRho@users.noreply.github.com>
2024-10-09 20:30:17 -0700

    added some ui and initial modal

--- drocsid/app/(setup)/page.tsx
+++ drocsid/app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -21,7 +22,7 @@ const SetupPage = async () => {
   if (server) return redirect("/servers/${server.id");
 
   // If no server is found, show this !
-  return <div>Create a Server</div>;
+  return <InitialModal/>
 };
 
 export default SetupPage;
