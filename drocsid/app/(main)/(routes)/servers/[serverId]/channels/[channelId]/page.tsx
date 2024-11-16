74fbfe0eee4b7354064aa3a9885f5bae2668e894
Alvin <alvinlhang@gmail.com>
2024-11-16 14:22:00 -0800

    Added chat upload and messaging functionality

--- drocsid/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ drocsid/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from "@/lib/current-profile";
 
 import { db } from "@/lib/db";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 interface ChannelIdPageProps {
     params: {
@@ -40,11 +41,22 @@ const ChannelIdPage = async ({
     }
 
     return ( 
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+        // changed h-full to h-screen, may need to change back
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
             <ChatHeader 
                 name={channel.name}
                 serverId={channel.serverId}
                 type="channel"
+            />
+            <div className="flex-1">Future Messages</div>
+            <ChatInput 
+                name={channel.name}
+                type="channel"
+                apiUrl="/api/socket/messages"
+                query={{
+                    channelId: channel.id,
+                    serverId: channel.serverId,
+                }}
             />
         </div>
     );
